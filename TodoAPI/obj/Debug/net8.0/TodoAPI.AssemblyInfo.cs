05dcaa154a96d23e416d0fd2145087c537322584
Huu Duc Do <dohuuduc_888@yahoo.com.vn>
2024-07-13 15:37:27 +0700

    add DbContext

--- TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
+++ TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9445c24da01a70ab759a8f641ca02cd0179323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f147502fffb8fa1f76ae9d84fcd35f5be711b772")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
