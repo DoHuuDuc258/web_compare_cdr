431db59536f5a7c46dda96b5567adb01b6e56e9f
Huu Duc Do <dohuuduc_888@yahoo.com.vn>
2024-07-13 16:23:37 +0700

    update_ver2

--- TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
+++ TodoAPI/obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f147502fffb8fa1f76ae9d84fcd35f5be711b772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a396755e874aed99fe1afb8a27841428f71956c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
